e42bef78d845c45eb2f5096291202668d802ffb8
Juan Parra <parrajuan1025@gmail.com>
2024-08-11 06:01:58 -0400

    updated path to service worker for built application

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,15 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('hey')
+    console.log('are you sure')
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    console.log('ho')
+    console.log('kikk')
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('Hey Ho')
+    console.log('okayy')
 });
