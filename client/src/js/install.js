e67f8406ba29852f3fb2c391f2b6a2d42cddac2f
Juan Parra <parrajuan1025@gmail.com>
2024-08-11 04:35:59 -0400

    source files, added css, js,  and html loader

---
+++ client/src/js/install.js
@@ -0,0 +1,17 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hey')
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    console.log('ho')
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('Hey Ho')
+});
---
+++ client/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'landing page'
+      })
+    ],
+// added loader for css
+module: {
+  rules: [
+    // ... existing rules
+    {
+      test: /\.css$/, // Matches all CSS files
+      use: ['style-loader', 'css-loader'] // Use style-loader and css-loader
+    },
+    {
+      test: /\.m?js$/, // Matches all Js files
+      exclude: /(node_modules | bower_components)/, //Ignore these
+      use:{
+        loader: "babel-loader",
+        options:{
+          presets:['@babel/preset-env']
+        }
+      }
+    },
+    // {
+    //   test: /\.html$/,
+    //   use:
+    // }
+  ]
+}
+  };
+};
